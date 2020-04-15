821f4029094345563089abe4c1bdb7f5a2e4148e
emmanuelsimango <emmanuelsimango@gmail.com>
2020-04-16 01:36:31 +0200

    animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComponentsModule } from './components/components.module';
 import { PagesModule } from './pages/pages.module';
-
+import AOS from 'aos'
 @NgModule({
   declarations: [
     AppComponent
@@ -14,9 +14,11 @@ import { PagesModule } from './pages/pages.module';
     BrowserModule,
     AppRoutingModule,
     ComponentsModule,
-    PagesModule
+    PagesModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  
+ }
