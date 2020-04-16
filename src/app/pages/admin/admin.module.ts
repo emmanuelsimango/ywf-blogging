5975d4517962ecfe0e29d3c67a7f3a2761313a26
emmanuelsimango <emmanuelsimango@gmail.com>
2020-04-16 23:15:57 +0200

    Login

--- src/app/pages/admin/admin.module.ts
+++ src/app/pages/admin/admin.module.ts
@@ -7,13 +7,15 @@ import { AdminBlogComponent } from './admin-blog/admin-blog.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { ComponentsModule } from 'src/app/components/components.module';
 
 
 @NgModule({
   declarations: [LoginComponent, AdminBlogComponent, ForgotPasswordComponent, SignUpComponent, DashboardComponent],
   imports: [
     CommonModule,
-    AdminRoutingModule
+    AdminRoutingModule,
+    ComponentsModule
   ]
 })
 export class AdminModule { }
